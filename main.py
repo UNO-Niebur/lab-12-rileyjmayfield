b1e239c98cbf2982fd5c4769334ccd6a5b1f4ad6
rileyjmayfield <rileyjmayfield@gmail.com>
2026-04-22 15:32:54 +0000

    Finished main.py and answered reflection

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 # SearchSortLab.py
-# Name:
-# Date:
+# Name: Riley Mayfield
+# Date: 4/22
 # Assignment: Lab 13 – Searching and Sorting
+# Purpose: Develop understanding of sorting lists
 
 
 def linearSearch(data, target):
     """Return the index of target if found, otherwise return -1."""
     
     # TODO: implement linear search
-    
+    for i in range(len(data)):
+        if data[i] == target:
+            return i
+
+
     return -1
 
 
@@ -16,6 +21,11 @@ def bubbleSort(data):
     """Sort the list using bubble sort and return the sorted list."""
     
     # TODO: implement bubble sort
+
+    for j in range(len(data)):
+        for i in range(len(data)-1):
+            if data[i] > data[i+1]:
+                data[i], data[i+1] = data[i+1], data[i]
     
     return data
 
